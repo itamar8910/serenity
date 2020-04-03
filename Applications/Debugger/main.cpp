3a641e71a7dc05bcabe6f4e82c8ddc8847411af9
Itamar <itamar8910@gmail.com>
2020-04-03 16:49:26 +0300

    Debugger: Catch debugee's breakpoint

--- Applications/Debugger/main.cpp
+++ Applications/Debugger/main.cpp
@@ -24,9 +24,100 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+#include <AK/Assertions.h>
+#include <LibC/sys/arch/i386/regs.h>
+#include <signal.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ptrace.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
-int main(int, char**)
+static int usage()
 {
-    printf("debugger\n");
+    printf("usage: sdb [command...]\n");
+    return 1;
+}
+
+static int g_pid = -1;
+
+static void handle_sigint(int)
+{
+    if (g_pid == -1)
+        return;
+
+    if (ptrace(PT_DETACH, g_pid, 0, 0) == -1) {
+        perror("detach");
+    }
+}
+
+void run_child_and_attach(char** argv)
+{
+    int pid = fork();
+
+    if (!pid) {
+        if (ptrace(PT_TRACE_ME, 0, 0, 0) == -1) {
+            perror("traceme");
+            return exit(1);
+        }
+
+        int rc = execvp(argv[1], &argv[1]);
+        if (rc < 0) {
+            perror("execvp");
+            exit(1);
+        }
+        ASSERT_NOT_REACHED();
+    }
+
+    if (waitpid(pid, nullptr, WSTOPPED) != pid) {
+        perror("waitpid");
+        exit(1);
+    }
+    g_pid = pid;
+}
+
+int main(int argc, char** argv)
+{
+    if (argc == 1)
+        return usage();
+
+    run_child_and_attach(argv);
+
+    struct sigaction sa;
+    memset(&sa, 0, sizeof(struct sigaction));
+    sa.sa_handler = handle_sigint;
+    sigaction(SIGINT, &sa, nullptr);
+
+    if (ptrace(PT_ATTACH, g_pid, 0, 0) == -1) {
+        perror("attach");
+        return 1;
+    }
+    if (waitpid(g_pid, nullptr, WSTOPPED) != g_pid) {
+        perror("waitpid");
+        return 1;
+    }
+
+    if (ptrace(PT_CONTINUE, g_pid, 0, 0) == -1) {
+        perror("continue");
+    }
+
+    // wait for breakpoint
+    if (waitpid(g_pid, nullptr, WSTOPPED) != g_pid) {
+        perror("waitpid");
+        return 1;
+    }
+
+    printf("hit breakpoint\n");
+
+    sleep(1);
+
+    if (ptrace(PT_CONTINUE, g_pid, 0, 0) == -1) {
+        perror("continue");
+    }
+
+    if (waitpid(g_pid, nullptr, WSTOPPED) != g_pid) {
+        perror("waitpid");
+        return 1;
+    }
 }
