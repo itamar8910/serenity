a14a74d9180f1f61114dbb778d7ae8b0b5e6b2e3
Itamar <itamar8910@gmail.com>
2020-06-27 16:53:22 +0300

    loader exists gracefully

--- Applications/Loader/main.cpp
+++ Applications/Loader/main.cpp
@@ -2,7 +2,6 @@
 
 void local_dbgputstr(const char* str, int len)
 {
-    // return str[0] + len;
     constexpr unsigned int function = Kernel::SC_dbgputch;
     for (int i = 0; i < len; ++i) {
         unsigned int result;
@@ -13,9 +12,20 @@ void local_dbgputstr(const char* str, int len)
     }
 }
 
+void exit(int code)
+{
+    constexpr unsigned int function = Kernel::SC_exit;
+    unsigned int result;
+    asm volatile("int $0x82"
+                 : "=a"(result)
+                 : "a"(function), "d"(code)
+                 : "memory");
+}
+
 int main()
 {
     const char str[] = "loader\n";
     local_dbgputstr(str, sizeof(str));
+    exit(0);
     return 0;
 }
