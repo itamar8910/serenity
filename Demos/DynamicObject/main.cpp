e1a6da74d28ced4e2b414ce4e06e8d10c78c2a58
Itamar <itamar8910@gmail.com>
2020-10-26 20:03:44 +0200

    run graphical app with shared libs!

--- Demos/DynamicObject/main.cpp
+++ Demos/DynamicObject/main.cpp
@@ -1,4 +1,11 @@
 #include "lib.h"
+#include <LibCore/Command.h>
+#include <LibGUI/Application.h>
+#include <LibGUI/BoxLayout.h>
+#include <LibGUI/Button.h>
+#include <LibGUI/Label.h>
+#include <LibGUI/Widget.h>
+#include <LibGUI/Window.h>
 #include <fcntl.h>
 #include <stdio.h>
 #include <sys/internals.h>
@@ -19,6 +26,34 @@ int main(int argc, char** argv, char** env)
         int _errno = errno;
         perror("open failed");
         printf("rc: %d, errno: %d\n", rc, _errno);
-        return func() + g_tls1 + g_tls2;
     }
+    printf("ls: %s\n", Core::command("ls", {}).characters());
+    auto app = GUI::Application::construct(argc, argv);
+
+    auto window = GUI::Window::construct();
+    window->resize(240, 160);
+    window->set_title("Hello World!");
+    window->set_icon(Gfx::Bitmap::load_from_file("/res/icons/16x16/app-hello-world.png"));
+
+    auto& main_widget = window->set_main_widget<GUI::Widget>();
+    main_widget.set_fill_with_background_color(true);
+    main_widget.set_background_color(Color::White);
+    auto& layout = main_widget.set_layout<GUI::VerticalBoxLayout>();
+    layout.set_margins({ 4, 4, 4, 4 });
+
+    auto& label = main_widget.add<GUI::Label>();
+    label.set_text("Hello\nWorld!");
+
+    auto& button = main_widget.add<GUI::Button>();
+    button.set_text("Good-bye");
+    button.set_size_policy(GUI::SizePolicy::Fill, GUI::SizePolicy::Fixed);
+    button.set_preferred_size(0, 20);
+    button.on_click = [&](auto) {
+        app->quit();
+    };
+
+    window->show();
+
+    return app->exec();
+    // return func() + g_tls1 + g_tls2;
 }
